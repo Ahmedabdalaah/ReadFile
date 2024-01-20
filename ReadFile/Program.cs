1d1211afa177d254b9e15f8b2138fd96affa91c0
Ahmed <ahmed.ismail14141414@gmail.com>
2024-01-20 02:23:35 +0200

    Add project files.

---
+++ ReadFile/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ReadFile;
+
+Read r;
+Console.WriteLine(" Enter the file name");
+
+String name = Console.ReadLine();
+if((name!=null))
+{
+    r = new Read();
+    r.readf(name);
+}
+else
+{
+    Console.WriteLine( " Invalid name");
+}
+
---
+++ ReadFile/Read.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadFile
+{
+    internal class Read
+    {
+        public   void readf(String name)
+        {
+            String s;
+            try
+            {
+                StreamReader str = new StreamReader(name);
+                 s= str.ReadLine();
+                while (s != null)
+                {
+                    Console.WriteLine(s);
+                    s = str.ReadLine();
+                }
+                str.Close();
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+
+        }
+    }
+}
